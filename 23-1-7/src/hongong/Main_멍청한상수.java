6132d8c23eb8019a27a4337fbae06f5cedac2856
Mr Park <parkeryangga@gmail.com>
2023-01-07 16:27:38 +0900

    집에서 문제 푼거

---
+++ 23-1-7/src/hongong/Main_멍청한상수.java
@@ -0,0 +1,35 @@
+package hongong;
+
+import java.util.Scanner;
+
+public class Main_멍청한상수 {
+
+	public static void main(String[]args) {
+	Scanner hi = new Scanner(System.in);
+		String num1 = hi.nextLine();
+		String num2 = hi.nextLine();
+		
+		char []arr1 = num1.toCharArray(); //  스트링 -> charAt
+		char []arr1re = new char[arr1.length];
+		for (int i=0; i<arr1.length; i++) {
+			arr1re[arr1.length-1-i] = arr1[i];
+		}
+		
+		String value = new String(arr1re);
+		System.out.println(arr1re);
+		
+		char []arr2 = num2.toCharArray(); //  스트링 -> charAt
+		char []arr2re = new char[arr2.length];
+		for (int i=0; i<arr1.length; i++) {
+			arr2re[arr2.length-1-i] = arr2[i];
+		}
+		String value2 = new String(arr2re);
+		System.out.println(arr2re);
+	
+		}
+		
+		
+	
+	}
+	
+}
---
+++ 23-1-7/src/hongong/자바의정석_주사위합.java
@@ -0,0 +1,48 @@
+package hongong;
+
+public class 자바의정석_주사위합 {
+	
+	public static void main(String[]args) {
+		
+		int dice1, dice2;
+		int [] count = new int[7];
+
+			while(true) {
+				
+				dice1 = (int)(Math.random()*6)+1;
+				dice2 = (int)(Math.random()*6)+1;
+				count[6]++;
+				
+				if(dice1==1) count[0]++;
+				if(dice2==1) count[0]++;
+
+				if(dice1==2) count[1]++;
+				if(dice2==2) count[1]++;
+
+				if(dice1==3) count[2]++;
+				if(dice2==3) count[2]++;
+				
+				if(dice1==4) count[3]++;
+				if(dice2==4) count[3]++;
+				
+				if(dice1==5) count[4]++;
+				if(dice2==5) count[4]++;
+				
+				if(dice1==6) count[5]++;
+				if(dice2==6) count[5]++;
+				
+				if((dice1+dice2)==5) break;
+
+			
+		}
+		
+		System.out.println("합이 5까지 주사위를 몇번 던 졌나요? " +count[6]);
+		System.out.println("1이 나온 횟수 " +count[0]);
+		System.out.println("2이 나온 횟수 " +count[1]);
+		System.out.println("3이 나온 횟수 " +count[2]);
+		System.out.println("4이 나온 횟수 " +count[3]);
+		System.out.println("5이 나온 횟수 " +count[4]);
+		System.out.println("6이 나온 횟수 " +count[5]);
+	}
+
+}
