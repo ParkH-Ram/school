8fd0cd5b6162bdfdb3bcde9be2e0845399b02386
Mr Park <parkeryangga@gmail.com>
2023-02-04 22:23:27 +0900

    23-2-4 술취해서 잠깐

---
+++ 23-2-4/src/Main_drinksoju.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class Main_drinksoju {
+
+		
+		public static void main(String[]args) {
+			String soju = "그는 술을 마심";
+			
+			
+			System.out.println(soju);
+			
+			String str;
+			
+			str = soju+ " 다이어트 중에는 술을 그만 마십니다";
+			
+			System.out.println(str);
+						
+			
+			
+		
+
+	}
+
+	
+}
