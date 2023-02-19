fbab18bc0237bb3d55af8a9fd58bad5a7c9e08fd
Mr Park <parkeryangga@gmail.com>
2023-02-19 19:49:55 +0900

    23-2-19

---
+++ 23-2-19/src/Main_개수세기_복습.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main_개수세기_복습 {
+	public static void main(String[]args) {
+		//  입력 받을 정수 총 수량 정하기 
+		//   정수 입력 받기
+		// 입력 받은 정수 중  입력한 정수랑 동일한 정수가 몇개인지 확인
+		// 출력 
+		
+	}
+
+}
